17839c7b0db8213ba5893a577bf0aa81cfc2118b
Kayla Cross <kac288@georgetown.edu>
2018-04-01 17:02:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,13 +2,17 @@ class Triangle
 
   attr_accessor :first, :second, :third
 
+  @sides = []
+
   def initialize(first, second, third)
-    @first = first
-    @second = second
-    @third = third
+    @sides << first << second << third
   end
 
   def kind
+    if !@first > 0 || !@second > 0 || !@third > 0
+      raise TriangleError
+    elsif @first+@second > @third
+
   end
 
   class TriangleError < StandardError
