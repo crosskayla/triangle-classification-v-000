e604a3b476666672e83b3d90c4f2eb4166cb9482
Kayla Cross <kac288@georgetown.edu>
2018-04-01 17:13:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
   def kind
     if @sides[1]+@sides[2] <= @sides[0] || @sides.detect{|x| x <= 0}
       raise TriangleError
-    elsif @sides[0] == @sides[1] && @sides[0] && @sides[2]
+    elsif @sides[0] == @sides[1] && @sides[0] == @sides[2]
       @kind = :equilateral
       binding.pry
     end
